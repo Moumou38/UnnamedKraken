fa08e31095dce52e79f84695faf38920c4180ac3
Moumou38 <moumou38@msn.com>
2016-05-24 13:33:36 +0200

    ink change and forgotten update

--- Assets/scripts/Player/InkBall.cs
+++ Assets/scripts/Player/InkBall.cs
@@ -5,16 +5,16 @@ public class InkBall : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f); 
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
         cpt += Time.deltaTime;
-        if (gameObject.transform.localScale.x < 5f)
-        {
-            gameObject.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f); 
-        }
+        //if (gameObject.transform.localScale.x < 5f)
+        //{
+        //    gameObject.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+        //}
         if (cpt > m_inkBallLifeSpan)
             GameObject.Destroy(gameObject); 
 	}
